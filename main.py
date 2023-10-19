cd67601192e7cf1067c6ab72e9909ca0be314b05
Pericles Feitoza <106848279+periclesanfe@users.noreply.github.com>
2023-10-19 23:01:14 +0000

    concluido

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 class HashTable:
     def __init__(self):
-        self._tamanho = 100 #
+        self._tamanho = 10 #
+        self.full = 0
         self._slots = [None] * self._tamanho
         self._valores = [None] * self._tamanho
+        self._descricoes = [None] * self._tamanho
     
     def hashfunction(self,chave):
         total = 0
@@ -13,7 +15,9 @@ class HashTable:
     def rehash(self,oldhash):
         return (oldhash+1) % self._tamanho
     
-    def put(self,chave,valor):
+    def put(self,chave,valor, desc):
+        if self.full == self._tamanho:
+            self.redimensionar()
         valor_hash = self.hashfunction(chave)
         
         while self._slots[valor_hash] != None and self._slots[valor_hash] != chave:
@@ -21,6 +25,8 @@ class HashTable:
             
         self._slots[valor_hash] = chave
         self._valores[valor_hash] = valor
+        self._descricoes[valor_hash] = desc
+        self.full += 1
                     
     def get(self,chave): # retorna a categoria do evento, por nome   
         valor_hash = self.hashfunction(chave)
@@ -33,7 +39,7 @@ class HashTable:
         return None
     
     def get_by_categoria(self, categoria):
-        eventos = [self._slots[i] for i in range(self._tamanho) if self._valores[i] == categoria]
+        eventos = [f'\n{self._slots[i]} ({self._descricoes[i]})' for i in range(self._tamanho) if self._valores[i] == categoria]
         return eventos
     
     def remove(self,chave):
@@ -43,43 +49,41 @@ class HashTable:
             if self._slots[valor_hash] == chave:
                 self._slots[valor_hash] = None
                 self._valores[valor_hash] = None
+                self._descricoes[valor_hash] = None
+                self.full -= 1
                 return True
             valor_hash = self.rehash(valor_hash)
         
         return False
     
     def redimensionar(self):
-        novo_tamanho = self._tamanho * 2
-        while not self.eh_primo(novo_tamanho):
-            novo_tamanho += 1
+        self._tamanho = self._tamanho * 2 + 1
+        while not self.eh_primo(self._tamanho):
+            self._tamanho += 1
         
-        novo_slots = [None] * novo_tamanho
-        novo_valores = [None] * novo_tamanho
+        novo_slots = [None] * self._tamanho
+        novo_valores = [None] * self._tamanho
+        novo_descricoes = [None] * self._tamanho
         
         for i in range(self._tamanho):
             if self._slots[i] != None:
-                novo_hash = self.hashfunction(self._slots[i]) % novo_tamanho
+                novo_hash = self.hashfunction(self._slots[i]) % self._tamanho
                 while novo_slots[novo_hash] != None:
-                    novo_hash = self.rehash(novo_hash) % novo_tamanho
+                    novo_hash = self.rehash(novo_hash) % self._tamanho
                 novo_slots[novo_hash] = self._slots[i]
                 novo_valores[novo_hash] = self._valores[i]
+                novo_descricoes[novo_hash] = self._descricoes[i]
         
-        self._tamanho = novo_tamanho
         self._slots = novo_slots
         self._valores = novo_valores
         
     def eh_primo(self,numero):
-        for i in range(2,numero):
+        for i in range(2,numero**0.5):
             if numero % i == 0:
                 return False
         return True
-    
-    def __getitem__(self,chave):
-        return self.get(chave)
-
-    def __setitem__(self,chave,valor):
-        self.put(chave,valor)
         
+
     def __str__(self):
         categorias = ""
         for categoria in self._valores:
@@ -97,7 +101,8 @@ def main():
         if escolha == "1":
             nome = input("Digite o nome do evento: ")
             categoria = input("Digite a categoria do evento: ")
-            eventos.put(nome,categoria)
+            descricao = input("Descreva o evento: ")
+            eventos.put(nome,categoria, descricao)
             print("Evento adicionado com sucesso!")
         
         elif escolha == "2":
@@ -105,13 +110,14 @@ def main():
             print(eventos)
             
         elif escolha == "3":
+            print(eventos)
             categoria = input("Digite a categoria do evento: ")
             eventos_categoria = eventos.get_by_categoria(categoria)
             
             if not eventos_categoria:
                 print(f"Nenhum evento encontrado na categoria '{categoria}'.")
             else:
-                print(f"Eventos na categoria '{categoria}': {', '.join(eventos_categoria)}")
+                print(f"Eventos na categoria '{categoria}': {', '.join(eventos_categoria)}\n")
 
 
         elif escolha == "4":
